18d3f8114e7ccfeaef0ac59ccd353a75fcb6cb09
Łukasz Tyczka <lukasz@tyczka.net>
2022-02-10 20:01:14 +0100

    Add Endpoints and stubdb

--- Program.cs
+++ Program.cs
@@ -1,18 +1,28 @@
 using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
+// comand to create project
+// dotnet new web -o PizzaStore -f net6.0 
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(iptions => { });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
 });
 var app = builder.Build();
+// comand to  install swagger.
+// dotnet add package Swashbuckle.AspNetCore --version 6.1.4
 app.UseSwagger();
 app.UseSwaggerUI(c =>
-  {
-     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
-  });
-app.MapGet("/", () => "Hello World234!");
+{
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+});
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", (int id) => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+
 
 app.Run();
