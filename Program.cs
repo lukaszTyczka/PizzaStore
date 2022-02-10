835447cc3836ba949bc7eba11489b028120ef34f
Łukasz Tyczka <lukasz@tyczka.net>
2022-02-10 20:31:16 +0100

    Add Ef core and set base stuff

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
 // comand to create project
 // dotnet new web -o PizzaStore -f net6.0 
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(iptions => { });
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDB>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
@@ -19,5 +22,5 @@ app.UseSwaggerUI(c =>
 });
 app.MapGet("/", () => "Hello world!");
 
-
+app.MapGet("/pizzas", async (PizzaDB db) => await db.Pizzas.ToListAsync());
 app.Run();
