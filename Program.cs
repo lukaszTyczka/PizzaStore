5f18488a7e6f00248208eba229baf432c30f41d2
Łukasz Tyczka <lukasz@tyczka.net>
2022-02-14 21:08:53 +0100

    Add sqlLite db

--- Program.cs
+++ Program.cs
@@ -5,9 +5,10 @@ using PizzaStore.Models;
 // dotnet new web -o PizzaStore -f net6.0 
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pizzas") ?? "Data Source=Pizzas.db";
 builder.Services.AddCors(iptions => { });
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<PizzaDB>(options => options.UseInMemoryDatabase("items"));
+builder.Services.AddSqlite<PizzaDB>(connectionString);
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
